d59bcf99fe3291d4334b68d40174697cddba1b5a
Jenna Pangilinan <122793994+yukinon19@users.noreply.github.com>
2023-01-23 11:43:21 +0800

    Add files via upload

---
+++ prelims/node-example.js
@@ -0,0 +1,13 @@
+// Name: Jenna Ella P. Pangilinan
+// Module: 3
+// Week 2 and 3: Basics of NodeJS
+
+// first program
+console.log("Welcome to WCSERVER!!");
+
+// Creating my first function
+function Display(x){
+    console.log(x);
+}
+
+Display(200);
