644ef6875030f62072bb46ad4438d36dacb62aa2
Jenna Pangilinan <122793994+yukinon19@users.noreply.github.com>
2023-01-23 20:47:10 +0800

    Add files via upload

---
+++ prelims/Lab-Activity-2/addNumbers/add.js
@@ -0,0 +1,9 @@
+// Jenna Ella P. Pangilinan
+// WD 201
+
+function add(num1,num2){
+    let x = num1 + num2;
+    return x;
+}
+
+module.exports = add;
---
+++ prelims/Lab-Activity-2/addNumbers/app.js
@@ -0,0 +1,6 @@
+// Jenna Ella P. Pangilinan
+// WD 201
+
+const add = require ('./add.js');
+
+console.log("The sum of the two numbers is " + add(2,9));
