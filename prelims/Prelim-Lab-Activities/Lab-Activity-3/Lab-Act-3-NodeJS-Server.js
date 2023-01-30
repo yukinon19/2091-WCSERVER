8da08eb917923b27f5584dcf762ba94c1e599080
yukinon19 <122793994+yukinon19@users.noreply.github.com>
2023-01-30 12:35:36 +0800

    1 file modified

--- prelims/Prelim-Lab-Activities/Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
+++ prelims/Prelim-Lab-Activities/Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
@@ -3,3 +3,45 @@
 //Section: WD 201
 
 // Use require() to import local module
+
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  if (req.url == '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.write(
+      '<html><body><h1>Welcome to my Node.js Application</h1></body></html>'
+    );
+    res.write(
+      '<html><body><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.write('<html><body><h1>This is the About Page</h1></body></html>');
+    res.write(
+      '<html><body><p>Hello John Smith. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.write('<html><body><h1>This is the Contact Page</h1></body></html>');
+    res.write(
+      '<html><body><p>John Smith, if you want additional details about this activity go to this site: https://www.tutorialteacher.com/nodejs/nodejs-tutorials</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.write('<html><body><h1>This is the Gallery Page</h1></body></html>');
+    res.end();
+  } else {
+    res.end('Invalid Request');
+  }
+});
+
+server.listen(5000);
+console.log('nagrrun naman yung server');
