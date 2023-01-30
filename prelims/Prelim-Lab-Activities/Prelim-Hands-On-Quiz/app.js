db29223ac3783cee380e464adac8f5c98dfab22f
yukinon19 <122793994+yukinon19@users.noreply.github.com>
2023-01-30 20:01:41 +0800

    1 file modified

--- prelims/Prelim-Lab-Activities/Prelim-Hands-On-Quiz/app.js
+++ prelims/Prelim-Lab-Activities/Prelim-Hands-On-Quiz/app.js
@@ -11,7 +11,7 @@ const workHR = calc.multiply(days,hrs);
 
 const gross = calc.multiply(workHR, rateHR);
 const tax = calc.multiply(gross,0.1);
-const sss = calc.divide(gross,6);
+const sss = calc.divide(gross,days);
 const pag_ibig = calc.divide(gross,workHR);
 const philH = 400;
 const deduct = calc.add(calc.add(tax,sss),calc.add(pag_ibig,philH));
