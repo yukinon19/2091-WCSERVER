fa0ef78143c6aea33c4ea5dc09fcd85c47a8285d
yukinon19 <122793994+yukinon19@users.noreply.github.com>
2023-02-08 15:40:21 +0800

    2 files added

---
+++ prelims/Prelim-Exam/app.js
@@ -0,0 +1,18 @@
+//Pangilinan, Jenna Ella P.
+
+var phrase = {
+  greet: function(name) {
+    return "Hello, " + name + "!";
+  },
+  
+  congratulations: function(name) {
+    return "Congratulations, " + name + "!";
+  },
+  
+  farewell: function(name) {
+    return "Goodbye, " + name + "!";
+  }
+
+}
+
+module.exports = phrase;
---
+++ prelims/Prelim-Exam/node.js
@@ -0,0 +1,9 @@
+//Pangilinan, Jenna Ella P.
+
+var phrase = require("./app.js");
+
+var name = "Jenna";
+
+console.log(phrase.greet(name));
+console.log(phrase.congratulations(name));
+console.log(phrase.farewell(name));
