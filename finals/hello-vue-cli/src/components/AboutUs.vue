3d0917c193fa5181466b7717523105b9eabdfa72
yukinon19 <122793994+yukinon19@users.noreply.github.com>
2023-04-19 23:52:38 +0800

    1 file added

---
+++ finals/hello-vue-cli/src/components/AboutUs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="about-us">
+    <div id="todo-card">
+      <div class="container">
+        <div class="alert alert-primary" role="alert">
+          <h1>{{ msg6 }}</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about-us',
+  data() {
+    return {
+      msg6: 'This is the About component',
+    };
+  },
+};
+</script>
